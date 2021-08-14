b1ca9b36c94a62b50174005e865b29d663a4792c
leoverde2 <leoboeri@hotmail.com>
2021-08-14 18:27:46 -0300

    Exercise 3.6

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
+const { response } = require("express")
 const express = require("express")
 const app = express()
 
 app.use(express.json())
 
-let persons = 
-[
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
+let persons =
+  [
+    {
+      "id": 1,
+      "name": "Arto Hellas",
+      "number": "040-123456"
+    },
+    {
+      "id": 2,
+      "name": "Ada Lovelace",
+      "number": "39-44-5323523"
+    },
+    {
+      "id": 3,
+      "name": "Dan Abramov",
+      "number": "12-43-234345"
+    },
+    {
+      "id": 4,
+      "name": "Mary Poppendieck",
+      "number": "39-23-6423122"
+    }
+  ]
 
 app.get('/api/persons', (request, response) => {
   response.json(persons)
@@ -35,7 +36,7 @@ app.get('/info', (request, response) => {
   const info = (
     `<p>Phonebook has info for ${persons.length} people</p>
     <p>${new Date()}</p>`
-    )
+  )
   response.send(info)
 })
 
@@ -54,6 +55,46 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
+})
+
+const generateId = () => {
+  return Math.floor(Math.random() * 1000000)
+}
+
+const errorHandler = (body) => {
+  if (!body.name) {
+    return 'name is missing'
+  }
+  else if (!body.number) {
+    return 'number is missing'
+  }
+  else if (persons.find(person => person.name === body.name)) {
+    return 'name must be unique'
+  }
+  else if (persons.find(person => person.number === body.number)) {
+    return 'number must be unique'
+  }
+  else return false
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  const error = errorHandler(body)
+  if (error !== false) {
+    return response.status(400).json({
+      error: error
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  }
+  persons = persons.concat(person)
+
+  response.json(person)
 })
 
 const PORT = 3001
